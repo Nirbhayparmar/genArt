bcd1830e2989eb220b6bd8380abb85f063e643fe
Nirbhay <niravnirbhay94@gmail.com>
2022-08-12 20:33:05 +0530

    added tweakpane in sketch6.js

--- sketch6.js
+++ sketch6.js
@@ -1,10 +1,22 @@
 const canvasSketch = require("canvas-sketch");
 const math = require("canvas-sketch-util/math");
 const random = require("canvas-sketch-util/random");
-
+import { Pane } from "tweakpane";
 const settings = {
 	dimensions: [1080, 1080],
 	animate: true,
+};
+const params = {
+	cols: 10,
+	rows: 10,
+	scaleMin: 1,
+	scaleMax: 5,
+	amplitude: 0.2,
+	frequency: 0.001,
+	animate: true,
+	frame: 0,
+	Noise2d: true,
+	linecap: "butt",
 };
 
 const sketch = () => {
@@ -12,8 +24,8 @@ const sketch = () => {
 		context.fillStyle = "white";
 		context.fillRect(0, 0, width, height);
 
-		const cols = 15;
-		const rows = 15;
+		const cols = params.cols;
+		const rows = params.rows;
 		const numCells = cols * rows;
 
 		const gridw = width * 0.8;
@@ -30,14 +42,24 @@ const sketch = () => {
 			const y = row * cellh;
 			const w = cellw * 0.8;
 			const h = cellh * 0.8;
-			const n = random.noise2D(x + frame * 15, y, 0.001);
-			const angle = n * Math.PI * 0.2;
+			const f = params.animate ? frame : params.frame;
+			const n = params.Noise2d
+				? random.noise2D(x + f * 15, y, params.frequency)
+				: random.noise3D(x, y, f * 15, params.frequency);
+			const angle = n * Math.PI * params.amplitude;
 			context.save();
 			context.translate(x, y);
 			context.translate(margx, margy);
 			context.translate(cellw * 0.5, cellh * 0.5);
 			context.rotate(angle);
-			context.lineWidth = math.mapRange(n, -1, 1, 1, 7);
+			context.lineWidth = math.mapRange(
+				n,
+				-1,
+				1,
+				params.scaleMin,
+				params.scaleMax
+			);
+			context.lineCap = params.linecap;
 			context.beginPath();
 			context.moveTo(-0.5 * w, 0);
 			context.lineTo(0.5 * w, 0);
@@ -55,5 +77,62 @@ const sketch = () => {
 		}
 	};
 };
+function createPane() {
+	const pane = new Pane();
+	const f1 = pane.addFolder({
+		title: "grid",
+	});
+	const f2 = pane.addFolder({
+		title: "noise",
+	});
+	const f3 = pane.addFolder({
+		title: "animateOrNot",
+	});
+	f1.addInput(params, "cols", {
+		min: 2,
+		max: 100,
+		step: 1,
+	});
+	f1.addInput(params, "rows", {
+		min: 2,
+		max: 100,
+		step: 1,
+	});
+	f1.addInput(params, "linecap", {
+		options: {
+			butt: "butt",
+			round: "round",
+			square: "square",
+		},
+	});
+	f1.addInput(params, "scaleMin", {
+		min: 1,
+		max: 5,
+		step: 0.5,
+	});
+	f1.addInput(params, "scaleMax", {
+		min: 6,
+		max: 20,
+		step: 0.5,
+	});
+	f2.addInput(params, "amplitude", {
+		min: 0.1,
+		max: 1,
+		step: 0.01,
+	});
+	f2.addInput(params, "frequency", {
+		min: -0.005,
+		max: 0.005,
+		step: 0.0001,
+	});
+	f2.addInput(params, "Noise2d");
+	f3.addInput(params, "animate");
+	f3.addInput(params, "frame", {
+		min: 0,
+		max: 999,
+		step: 1,
+	});
+}
 
 canvasSketch(sketch, settings);
+createPane();
